37da18e11b8474b97cbe0e90e98ce50cb19181b6
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-02-21 14:15:41 -0600

    route

--- ws.js
+++ ws.js
@@ -10,7 +10,7 @@ const wss = new WebSocket.Server({ server }, () => {
 
 var clients = [];
 
-app.get('/salmom', function(req, res){
+app.get('/', function(req, res){
     res.sendfile('start.html');
 });
 

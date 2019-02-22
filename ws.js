800774ec235df04d07a0a5a2d35d0b15dbc450d1
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-02-22 14:54:01 -0600

    change id generation from ip to uuid

--- ws.js
+++ ws.js
@@ -2,6 +2,7 @@ const express = require('express');
 const WebSocketServer = require('ws').Server;
 const https = require('http');
 const fs = require('fs');
+const uuid = require('short-uuid');
 var port = 8080;
 
 var options = {
@@ -23,11 +24,14 @@ sServer.listen(port, () => {
 var wss = new WebSocketServer({ server: sServer });
 
 var clients = [];
+var clientIds = [];
 
 wss.on('connection', function connection(ws, req) {
   console.log("CONNECTION");
-
   clients.push(ws);
+
+  let clientId = uuid.generate();
+  clientIds.push(clientId);
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
@@ -37,9 +41,8 @@ wss.on('connection', function connection(ws, req) {
       if(clients[i].readyState === 1){
         let json = JSON.stringify({
           position: message.toString().split("_")[0],
-          id: req.connection.remoteAddress
+          id: clientId[i];
         });
-        console.log(json);
         clients[i].send(json);
       }
     }
